5a069e338fdc120d3e6f708cf8ebd03e3a608bef
Green_Sprout <sijun0514@naver.com>
2022-03-14 19:17:37 +0900

    UI 상호작용 변경

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -6,6 +6,8 @@ using DG.Tweening;
 public class CameraManager : MonoBehaviour
 {
     public Transform look_ob;
+    public GameObject joy_s;
+    public GameObject UI_mo;
 
     public Vector3 lookOffset = new Vector3(0, 90f, -73f);
     private void LateUpdate()
@@ -18,10 +20,15 @@ public class CameraManager : MonoBehaviour
     {
         SoundManager.instance.PlaySE("UI Click Sound");
         DOTween.To(() => Camera.main.fieldOfView, x=> Camera.main.fieldOfView = x, 20, 2);
+        joy_s.SetActive(true);
+        UI_mo.SetActive(false);
+
     }
     public void Hide_On_Click()
     {
         SoundManager.instance.PlaySE("UI Click Sound");
         DOTween.To(() => Camera.main.fieldOfView, x => Camera.main.fieldOfView = x, 20, 2);
+        joy_s.SetActive(true);
+        UI_mo.SetActive(false);
     }
 }
