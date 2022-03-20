932a4f3144de4ac760acf9df520970d91be9b82f
Green_Sprout <sijun0514@naver.com>
2022-03-20 14:07:01 +0900

    시작화면 신과 Tryangle로고

---
+++ Assets/Script/Sencesprit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sencesprit : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void Go_To_Gmae_Sence()
+    {
+        SceneManager.LoadScene("New Scene");
+    }
+}
