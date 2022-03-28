dc8109470b591915a15bbf7dff2f9a9689ff7b3b
Green_Sprout <sijun0514@naver.com>
2022-03-28 10:54:04 +0900

    캐릭터 대한 연구

--- Assets/Script/Joysprit.cs
+++ Assets/Script/Joysprit.cs
@@ -7,27 +7,33 @@ public class Joysprit : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 {
     [SerializeField] private RectTransform rect_Background;
     [SerializeField] private RectTransform rect_Joystick;
+    [SerializeField] private CharacterController controller;
 
     private float radius;
 
     [SerializeField] private GameObject go_Plaayer;
     [SerializeField] private float moveSpeed;
 
-    private bool isTouch = false;
+    //private bool isTouch = false;
     private Vector3 movePosition;
+    public Animator anim;
 
-    void Update()
-    {
-        if (isTouch)
-            go_Plaayer.transform.position += movePosition;
-    }
-
-    void Start()
+    private void Start()
     {
         radius = rect_Background.rect.width * 0.5f;
     }
+
+    void Update()
+    {
+        // if (isTouch)
+        //go_Plaayer.transform.position += movePosition;
+        controller.Move(movePosition * Time.deltaTime * moveSpeed);
+    }
+
+  
     public void OnDrag(PointerEventData eventData)
     {
+
         Vector2 value = eventData.position - (Vector2)rect_Background.position;
 
         value = Vector2.ClampMagnitude(value, radius);
@@ -35,16 +41,23 @@ public class Joysprit : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         Debug.Log("비정규화" + value);
         value = value.normalized;
         Debug.Log("정규화" + value);
-        movePosition = new Vector3(value.x * moveSpeed * Time.deltaTime, 0f, value.y * moveSpeed * Time.deltaTime);
+        movePosition = new Vector3(value.x , 0f, value.y);
+        
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        isTouch = true;
+        //isTouch = true;
+        anim.SetBool("player walk", true);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         rect_Joystick.localPosition = Vector3.zero;
+        movePosition = Vector3.zero;
+        anim.SetBool("player walk", false);
     }
+
+
 }
