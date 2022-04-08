07aca9df94fa55be189b1c623edf7444d7e8e248
Green_Sprout <sijun0514@naver.com>
2022-04-08 10:29:00 +0900

    test2

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -10,14 +10,25 @@ public class CameraManager : MonoBehaviour
     public GameObject UI_mo;
 
     public Vector3 lookOffset = new Vector3(0, 90f, -73f);
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0) + lookOffset;
+        
+
+        transform.LookAt(GameObject.Find("Map").GetComponent<Transform>());
+    }
     private void LateUpdate()
     {
-        transform.position = look_ob.position + lookOffset;
-
-        transform.LookAt(look_ob);
+        if(Camera.main.fieldOfView < 50)
+        {
+            transform.position = look_ob.position + lookOffset;
+            transform.LookAt(look_ob);
+        }
     }
     public void Seek_On_Click()
     {
+        
         SoundManager.instance.PlaySE("UI Click Sound");
         DOTween.To(() => Camera.main.fieldOfView, x=> Camera.main.fieldOfView = x, 20, 2);
         onPointer.SetActive(true);
