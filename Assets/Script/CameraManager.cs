1ae15ee9f4e197cf46d3fced04bb992272055648
Green_Sprout <sijun0514@naver.com>
2022-04-15 10:30:19 +0900

    2022-04-15

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class CameraManager : MonoBehaviour
 {
     public Transform look_ob;
     public GameObject onPointer;
     public GameObject UI_mo;
+    public TextMeshProUGUI delayGameStrat;
+    private int Delay = 3;
 
     public Vector3 lookOffset = new Vector3(0, 90f, -73f);
 
@@ -30,9 +33,11 @@ public class CameraManager : MonoBehaviour
     {
         
         SoundManager.instance.PlaySE("UI Click Sound");
-        DOTween.To(() => Camera.main.fieldOfView, x=> Camera.main.fieldOfView = x, 20, 2);
-        onPointer.SetActive(true);
+        DOTween.To(() => Camera.main.fieldOfView, x=> Camera.main.fieldOfView = x, 7.5f, 2);
+        look_ob.transform.DOScale(new Vector3(15, 15, 15), 2);
         UI_mo.SetActive(false);
+        StartCoroutine(Gamestrat());
+        Delay_Strat();
 
     }
     public void Hide_On_Click()
@@ -41,5 +46,23 @@ public class CameraManager : MonoBehaviour
         DOTween.To(() => Camera.main.fieldOfView, x => Camera.main.fieldOfView = x, 20, 2);
         onPointer.SetActive(true);
         UI_mo.SetActive(false);
+        Delay_Strat();
+    }
+
+    public void Delay_Strat()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            delayGameStrat.text = Delay.ToString();
+            delayGameStrat.GetComponent<RectTransform>().DOScale(new Vector3(0, 0, 0), 1).From().SetEase(Ease.OutBounce).SetDelay(i);
+            Delay--;
+        }
+    }
+
+    IEnumerator Gamestrat()
+    {
+        yield return new WaitForSeconds(3);
+        DOTween.To(() => Camera.main.fieldOfView, x => Camera.main.fieldOfView = x, 20, 2);
+        onPointer.SetActive(true);
     }
 }
