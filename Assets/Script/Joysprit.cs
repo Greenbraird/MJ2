1908d55553d0119ff9c77e4a76317852c73d7d74
Green_Sprout <sijun0514@naver.com>
2022-04-27 22:16:07 +0900

    2022-04-27

--- Assets/Script/Joysprit.cs
+++ Assets/Script/Joysprit.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
+
 public class Joysprit : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     [SerializeField] private RectTransform rect_Background;
@@ -25,34 +27,33 @@ public class Joysprit : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
         radius = rect_Background.rect.width * 0.5f;
         isTouch = false;
         playerTransform = controller.GetComponent<Transform>();
+
     }
 
     void Update()
     {
+
         if (isTouch)
             controller.Move(movePosition * Time.deltaTime * moveSpeed);
 
         playerTransform.transform.rotation = Quaternion.Euler(0, playerRotation, 0);
     }
 
-  
+
+
     public void OnDrag(PointerEventData eventData)
     {
         //Player Cha y값 회전에 대한 코드
         Vector2 v2 = rect_Joystick.position - rect_Background.position;
         playerRotation = Mathf.Atan2(v2.x, v2.y) * Mathf.Rad2Deg;
-        Debug.Log(eventData.GetType().Name);
         //Player Char의 움직임에 대한 코드
         anim.SetBool("player walk", true);
         Vector2 value = eventData.position - (Vector2)rect_Background.position;
 
         value = Vector2.ClampMagnitude(value, radius);
         rect_Joystick.localPosition = value;
-        //Debug.Log("비정규화" + value);
 
         value = value.normalized;
-
-        //Debug.Log("정규화" + value);
         movePosition = new Vector3(value.x , 0f, value.y);
         
 
